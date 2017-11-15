e554832d6dc49a4f5763730978d13d8adfa05035
nWrightSB <nathan.wright@smartbear.com>
2017-11-15 16:56:23 -0500

    checks current_user is not null, returns status message"

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Login = props => {
   let current_user = props.current_user;
   let current_pw = props.current_pw;
+  let status = "";
+
+  if (current_user == null || current_pw === null) {
+    status = "Please Login"
+  } else {
+    status = "Logged In as " + current_user
+  }
+
+  return (
+    <div className="login_container">
+      {status}
+    </div>
+  )
 }
 
 export default Login;
