c8a0dffa3f692246dee59c8fec37f7c89e5526cf
nWrightSB <nathan.wright@smartbear.com>
2018-01-03 13:36:29 -0500

    commit before redux transition

--- src/App.js
+++ src/App.js
@@ -1,146 +1,10 @@
-import React, { Component } from 'react';
-import Login from './Login'
-import Executions from './Executions'
+import React from 'react'
 import Navigation from './Navigation'
-import './App.css';
-import './ProjectExecution.css'
-import './TestSuiteExecution.css'
-import request from 'request'
+import Login from './Login'
+import Main from './Main'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // USER AND SESSION VALUES
-      current_user: null,
-      current_pw: null,
-      test_server: 'http://10.0.30.175:8080',
-      // VIEW DATA
-      data_executions: null,
-      data_project: null,
-      data_executionID: null,
-      data_testsuite: null,
-      data_testcase: null,
-      data_teststep: null,
-      data_messages: null
-    }
-    this.handleLogin = this.handleLogin.bind(this)
-    this.handleLogout = this.handleLogout.bind(this)
-    this.handleProjectDetails = this.handleProjectDetails.bind(this)
-    this.handleTestSuiteDetails = this.handleTestSuiteDetails.bind(this)
-    this.handleTestCaseDetails = this.handleTestCaseDetails.bind(this)
-    this.handleTestStepDetails = this.handleTestStepDetails.bind(this)
-  }
-
-  handleProjectDetails(executionID) {
-    let projectData = JSON.parse(this.state.data_executions)
-
-    for (let i = 0; i < projectData["projectResultReports"].length; i++){
-      let current_run = projectData["projectResultReports"][i]
-      if (current_run["executionID"].toString() === executionID.toString()) {
-        this.setState({
-          data_project: current_run,
-          data_testsuite: null,
-          data_testcase: null,
-          data_teststep: null,
-          data_messages: null
-        })
-      }
-    }
-  }
-
-  handleTestSuiteDetails(executionID, testsuite_name) {
-    let projectData = JSON.parse(this.state.data_executions)
-
-    for (let i = 0; i < projectData["projectResultReports"].length; i++){
-      let current_project_run = projectData["projectResultReports"][i]
-      if (current_project_run["executionID"].toString() === executionID.toString()) {
-        for (let n = 0; n < current_project_run["testSuiteResultReports"].length; n++) {
-            let current_testsuite_run = current_project_run["testSuiteResultReports"][n]
-            if (current_testsuite_run["testSuiteName"] === testsuite_name) {
-              this.setState({
-                data_testsuite: current_testsuite_run,
-                data_executionID: executionID,
-                data_testcase: null,
-                data_teststep: null,
-                data_messages: null
-              })
-            }
-        }
-      }
-    }
-  }
-
-  handleTestCaseDetails(executionID, testsuite_name, testcase_name){
-    let data_testsuite = this.state.data_testsuite
-
-    for (let i = 0; i < data_testsuite["testCaseResultReports"].length; i++) {
-      let current_testcase = data_testsuite["testCaseResultReports"][i]
-      if (testcase_name === current_testcase["testCaseName"]) {
-        this.setState({
-          data_testcase: current_testcase,
-          data_executionID: executionID,
-          data_teststep: null
-        })
-      }
-    }
-  }
-
-  handleTestStepDetails(teststep_name, transactionID, messages){
-    let reqObj = {
-      url: this.state.test_server + '/v1/readyapi/executions/' + this.state.data_executionID + "/transactions/" + transactionID,
-      auth: {
-          'user': this.state.current_user,
-          'pass': this.state.current_pw
-      }
-    };
-
-    request(reqObj, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-          this.setState({
-            data_teststep: body,
-            data_messages: messages
-          })
-      }
-    }.bind(this));
-  }
-
-  handleLogin(username, password) {
-    let reqObj = {
-      url: this.state.test_server + '/v1/readyapi/executions',
-      auth: {
-          'user': username,
-          'pass': password
-      }
-    };
-
-    request(reqObj, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-          this.setState({
-            current_user: reqObj.auth.user,
-            current_pw: reqObj.auth.pass,
-            data_executions: body
-          })
-      }
-    }.bind(this));
-  }
-
-  handleLogout() {
-    this.setState({
-      current_user: null,
-      current_pw: null,
-      data_executions: null,
-      data_project: null,
-      data_executionID: null,
-      data_testsuite: null,
-      data_testcase: null,
-      data_teststep: null
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
+const App = () => {
+    <div>
         <header className="App-header">
           <Navigation 
             app_name={"ts_ui"}
@@ -152,23 +16,7 @@ class App extends Component {
             handleLogout={this.handleLogout}
           />
         </header>
-        <br />
-        <Executions
-          data_executions={this.state.data_executions}
-          data_project={this.state.data_project}
-          data_testsuite={this.state.data_testsuite}
-          data_executionID={this.state.data_executionID}
-          data_testcase={this.state.data_testcase}
-          data_teststep={this.state.data_teststep}
-          data_messages={this.state.data_messages}
-          handleProjectDetails={this.handleProjectDetails}
-          handleTestSuiteDetails={this.handleTestSuiteDetails}
-          handleTestCaseDetails={this.handleTestCaseDetails}
-          handleTestStepDetails={this.handleTestStepDetails}
-        />
-      </div>
-    );
-  }
+    </div>
 }
 
-export default App;
+export default App
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Navigation = props => {
   let app_name = props.app_name
 
   return (
     <div className="nav_container">
-      {app_name}
+        {app_name}
+        <ul>
+            <li><Link to="/executions">Executions</Link></li>
+        </ul>
     </div>
   )
 }
